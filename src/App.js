fde902e00d21e4c062de2dcdcb2c54f9b2ef2138
Raiyad Raad <raiyad.raad@gmail.com>
2021-03-17 04:46:57 +0600

    create product component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@
 // Author: raadu
 // Date: 16 March 2021, 9:15PM
 
+import { useState } from 'react';
+import Products from "./components/Products/Products";
+import data from './data/productList.json';
+
+// Dependencies
+
 function App() {
+  // State
+  const [products, setProducts] = useState(data.products);
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">Food Delivery App</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products
+              products={products}
+            />
+          </div>
+          <div className="sidebar">
+            Cart Items
+          </div>
+        </div>
       </main>
       <footer>
         All rights is reserved
